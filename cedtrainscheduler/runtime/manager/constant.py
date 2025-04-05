34c85a61c25f3a04d0755924feac61367e103dce
l1hy <1151824226@qq.com>
2025-04-05 12:33:02 +0800

    fix

--- cedtrainscheduler/runtime/manager/constant.py
+++ cedtrainscheduler/runtime/manager/constant.py
@@ -7,8 +7,8 @@ INTRA_DOMAIN_BANDWIDTH = 200
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 CONFIG_DIR = os.path.join(BASE_DIR, "config")
-FS_CONFIG_PATH = os.path.join(CONFIG_DIR, "single_node_fs_config.json")
+FS_CONFIG_PATH = os.path.join(CONFIG_DIR, "multi_node_fs_config.json")
 
 
-RESULT_DIR = os.path.join(BASE_DIR, "result")
+RESULT_DIR = os.path.join(BASE_DIR, "metrics")
 TASK_RECORD_SAVE_PATH = os.path.join(RESULT_DIR, "task_record.json")
